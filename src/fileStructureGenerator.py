e2d7f70a1b351fb408043332fa03155e0aeee0ba
Michal Keda <adekcz@gmail.com>
2023-01-10 14:36:43 +0100

    adding utf8 encoding of generated files

--- src/fileStructureGenerator.py
+++ src/fileStructureGenerator.py
@@ -10,8 +10,21 @@ def generate_html_file(path, file_name):
                 file_path = os.path.join(root, file)
                 file_path = utils.remove_prefix(file_path, path)
                 file_path = file_path.replace("\\", "/")
-                html += "<a href=\"./" + file_path + "\">" + file_path + "</a><br/>"
+                html += "<a href=\"./" + file_path + "\">" + file_path + "</a><br/>\n"
 
     #write to file
     with open(file_name, "w", encoding="utf-8") as file:
-        file.write(html)
+        file.write(html_template().format(content=html))
+
+def html_template():
+    return """<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>index.html</title>
+</head>
+<body>
+{content}
+</body>
+</html>
+"""
--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ import ftp
 
 if __name__ == "__main__":
     path = "D:\\Dropbox\\Apps\\Obsidian\\Public\\www"
-    #run_command_on_files(path, "pandoc", "-t html -o", ".html")
+    run_command_on_files(path, "pandoc", "-s -t html -o ", ".html")
     path_index = path+ os.sep +"index.html"
     fileStructureGenerator.generate_html_file(path, path_index)
     ftp.upload_files(path)
